31396dadf964577047fbdfbdf3639dea8b0b7f35
shevapato2008 <shevapato2008@gmail.com>
2016-12-30 05:28:57 -0500

    Initial Commit.

---
+++ 15. Multithreading in Swing with SwingWorker/src/App.java
@@ -0,0 +1,19 @@
+import javax.swing.SwingUtilities;
+
+
+public class App {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            
+            @Override
+            public void run() {
+                new MainFrame("SwingWorker Demo");
+            }
+        });
+    }
+
+}
